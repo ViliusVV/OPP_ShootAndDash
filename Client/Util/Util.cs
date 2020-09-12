5f5796ec6a2c267bef4511ae2fa689da0801eccf
Vilius <vilius.valinskis@ktu.edu>
2020-09-12 19:48:48 +0300

    Fix cursor shift when moving

---
+++ Client/Util/Util.cs
@@ -0,0 +1,25 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.Util
+{
+    static class Util
+    {
+        public static Vector2f GetSpriteCenter(Sprite sprite)
+        {
+            Vector2f size = GetSpriteSize(sprite);
+            return new Vector2f(size.X / 2.0f, size.Y / 2.0f);
+        }
+
+        public static Vector2f GetSpriteSize(Sprite sprite)
+        {
+            float xSize = sprite.Scale.X * sprite.Texture.Size.X;
+            float ySize = sprite.Scale.Y * sprite.Texture.Size.Y;
+
+            return new Vector2f(xSize, ySize);
+        }
+    }
+}
