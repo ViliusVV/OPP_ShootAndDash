7cec8e47aba824e74b36c24700fafe88113430f0
Rocco <rokas.kran@gmail.com>
2020-09-28 18:15:31 +0300

    add empty server tests

---
+++ ServerXUnitTests/ServerTests.cs
@@ -0,0 +1,36 @@
+using Server.Hubs;
+using System;
+using Xunit;
+using Microsoft.AspNetCore.SignalR;
+using Moq;
+using Server;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ServerXUnitTests
+{
+    public class ServerTests : ShotAndDashHub
+    {
+        public Mock<GameManager> GameManager { get; private set; }
+
+        public ServerTests(Mock<GameManager> gameManager)
+        {
+
+            var mockConnection = new Mock<HubConnection>();
+        }
+        //[Fact]
+        //public void HubsAreMockableViaDynamic()
+        //{
+        //    bool sendCalled = false;
+        //    ShotAndDashHub hub = new ShotAndDashHub();
+        //    var mockClients = new Mock<IHubCallerConnectionContext<dynamic>>();
+        //    hub.Clients = mockClients.Object;
+        //    dynamic all = new ExpandoObject();
+        //    all.broadcastMessage = new Action<string, string>((name, message) => {
+        //        sendCalled = true;
+        //    });
+        //    mockClients.Setup(m => m.All).Returns((ExpandoObject)all);
+        //    hub.Send("TestUser", "TestMessage");
+        //    Assert.True(sendCalled);
+        //}
+    }
+}
