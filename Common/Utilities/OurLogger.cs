ba640ca224ef74bb6708394b307405f64909597d
Vilius <vilius.valinskis@ktu.edu>
2020-10-11 19:29:34 +0300

    Logger util

---
+++ Common/Utilities/OurLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Common.Utilities
+{
+    class OurLogger
+    {
+        public static readonly OurLogger _ourLogger = new OurLogger();
+
+        private OurLogger() { }
+
+        public OurLogger GetInstance()
+        {
+            return _ourLogger;
+        }
+
+        public static void Log(string text, [CallerFilePath] string file = "", [CallerMemberName] string member = "", [CallerLineNumber] int line = 0)
+        {
+            Console.WriteLine("{0}_{1}({2}): {3}", Path.GetFileName(file), member, line, text);
+        }
+    }
+}
