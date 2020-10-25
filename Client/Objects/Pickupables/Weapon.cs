4abe2366332ca54023a1f3c170159859174c2e3c
Margir <martynas.girdziuna@gmail.com>
2020-10-25 15:25:48 +0200

    Command Finish (mute/unmute)

--- Client/Objects/Pickupables/Weapon.cs
+++ Client/Objects/Pickupables/Weapon.cs
@@ -37,6 +37,7 @@ namespace Client.Objects
         public Clock ReloadCooldown { get; set; } = new Clock();
         public Sprite ProjectileSprite { get; set; }
         public Sprite LaserSprite { get; set; }
+        
 
         public Weapon()
         {
@@ -86,13 +87,24 @@ namespace Client.Objects
         }
         public override void Pickup(Player player)
         {
-            for (int i = 1; i < player.HoldingWeapon.Length; i++)
+            bool check = false;
+            for (int i = 0; i < 3; i++)
             {
-                if(player.HoldingWeapon[i] == null)
-				{
-                    player.SetWeapon(this);
-                    player.HoldingWeapon[i] = this;
-                    break;
+                if (player.HoldingWeapon[i] != null && player.HoldingWeapon[i].Name == this.Name)
+                {
+                    check = true;
+                }
+            }
+            if (check == false)
+            {
+                for (int i = 1; i < player.HoldingWeapon.Length; i++)
+                {
+                    if (player.HoldingWeapon[i] == null)
+                    {
+                        player.SetWeapon(this);
+                        player.HoldingWeapon[i] = this;
+                        break;
+                    }
                 }
             }
         }
@@ -118,6 +130,7 @@ namespace Client.Objects
                 ChangeAmmo(-1);
 
                 Sound sound = SoundHolder.GetInstance().Get(SoundIdentifier.GenericGun);
+                sound.Volume = SoundVolume.GetInstance().GetVolume();
                 sound.Play();
             }
         }
@@ -143,13 +156,14 @@ namespace Client.Objects
 
                             collidables.RemoveAt(j);
                             Clock explosionDuration = new Clock();
+                            //Console.WriteLine(explosionDuration.ElapsedTime.AsMilliseconds());
                             //while (true)
                             //{
-                            //    if (explosionDuration.ElapsedTime.AsSeconds() > 0.5f)
-                            //    {
-                            //        GameState.GetInstance().NonCollidables.Remove(explosion);
-                            //        break;
-                            //    }
+                            //if (explosionDuration.ElapsedTime.AsMilliseconds() > 300)
+                            //{
+                            //    GameState.GetInstance().NonCollidables.Remove(explosion);
+                            //    //break;
+                            //}
                             //}
                             //GameState.GetInstance().NonCollidables.Remove(explosion);
 
@@ -227,6 +241,7 @@ namespace Client.Objects
             if (this.Ammo != this.MagazineSize && this.Reloading != true)
             {
                 Sound sound = SoundHolder.GetInstance().Get(SoundIdentifier.Reload);
+                sound.Volume = SoundVolume.GetInstance().GetVolume();
                 sound.Play();
 
                 this.ChangeAmmo(-this.Ammo);
---
+++ Client/Utilities/SoundVolume.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.Utilities
+{
+    class SoundVolume
+    {
+        private static SoundVolume _instance = new SoundVolume();
+        private static float Volume = 50;
+
+        public static SoundVolume GetInstance()
+        {
+            return _instance;
+        }
+        public void SetVolume(float i)
+        {
+            Volume = i;
+        }
+
+        public float GetVolume()
+        {
+            return Volume;
+        }
+        
+    }
+}
