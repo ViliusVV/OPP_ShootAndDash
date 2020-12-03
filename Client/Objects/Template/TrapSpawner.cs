f78b442e068267b8a1056d1a413d48703ce5b765
Airidas <airidas12111@gmail.com>
2020-12-03 18:03:22 +0200

    Template method PlayerKnockback

--- Client/Objects/Template/TrapSpawner.cs
+++ Client/Objects/Template/TrapSpawner.cs
@@ -14,15 +14,52 @@ namespace Client.Objects.Template
 
         public void BuildTrap()
         {
-            ApplySkin(); // think of something else? 
+            ApplySkin();
         }
 
         public override void Pickup(Player player)
         {
             this.PickedUp = true;
+            PlayerKnockback(player);
 
             ApplyDamage(player);
             ApplyBehavior(player);
+        }
+
+        void PlayerKnockback(Player player)
+        {
+            if (player.Speed.X > 0 && player.Speed.Y == 0) // Left 
+            {
+                player.Speed = new SFML.System.Vector2f(player.Speed.X - 20f, 0f);
+            }
+            else if (player.Speed.X < 0 && player.Speed.Y == 0) // Right 
+            {
+                player.Speed = new SFML.System.Vector2f(player.Speed.X + 20f, 0f);
+            }
+            else if (player.Speed.X == 0 && player.Speed.Y > 0) // Top
+            {
+                player.Speed = new SFML.System.Vector2f(0f, player.Speed.Y - 20f);
+            }
+            else if (player.Speed.X == 0 && player.Speed.Y < 0) // Bottom
+            {
+                player.Speed = new SFML.System.Vector2f(0f, player.Speed.Y + 20f);
+            }
+            else if (player.Speed.X > 0 && player.Speed.Y > 0) // Top Left
+            {
+                player.Speed = new SFML.System.Vector2f(player.Speed.X - 20f, player.Speed.Y - 20f);
+            }
+            else if (player.Speed.X < 0 && player.Speed.Y > 0) // Top Right
+            {
+                player.Speed = new SFML.System.Vector2f(player.Speed.X + 20f, player.Speed.Y - 20f);
+            }
+            else if (player.Speed.X > 0 && player.Speed.Y < 0) // Bottom Left
+            {
+                player.Speed = new SFML.System.Vector2f(player.Speed.X - 20f, player.Speed.Y + 20f);
+            }
+            else if (player.Speed.X < 0 && player.Speed.Y < 0) // Bottom Right
+            {
+                player.Speed = new SFML.System.Vector2f(player.Speed.X + 20f, player.Speed.Y + 20f);
+            }
         }
     }
 }
